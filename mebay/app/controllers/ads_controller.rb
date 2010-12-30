85f0ce966876e50a83f3cb83c69d280abe586572
tanabe <tanabe@ababa.local>
2010-12-31 01:49:58 +0900

    implements create action

--- mebay/app/controllers/ads_controller.rb
+++ mebay/app/controllers/ads_controller.rb
@@ -2,9 +2,17 @@ class AdsController < ApplicationController
   def new
     @ad = Ad.new
   end
+
+  def create
+    @ad = Ad.new(params[:ad])
+    @ad.save
+    redirect_to "/ads/#{@ad.id}"
+  end
+
   def index
     @ads = Ad.all
   end
+
   def show
     @ad = Ad.find(params[:id])
   end
