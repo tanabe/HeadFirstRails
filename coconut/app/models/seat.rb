3e4aa9dd1e8850860478f7515d1bb6dad18b1889
tanabe <tanablog@gmail.com>
2010-12-31 21:09:01 +0900

    using belongs_to

--- coconut/app/models/seat.rb
+++ coconut/app/models/seat.rb
@@ -1,2 +1,8 @@
 class Seat < ActiveRecord::Base
+  belongs_to :flight
+  def validate
+    if baggage > flight.baggage_allowance
+      errors.add_to_base("You have too much baggage")
+    end
+  end
 end
